7ce024ab0aa8b4db5880458ffb52ca46e9aced82
Alberto <betim009@gmail.com>
2022-10-02 12:23:20 -0300

    req 02

---
+++ desafio2.sql
@@ -0,0 +1,4 @@
+SELECT
+    (SElECT COUNT(*) FROM SpotifyClone.cancoes) AS 'cancoes',
+    (SElECT COUNT(*) FROM SpotifyClone.artistas) AS 'artistas',
+    (SElECT COUNT(*) FROM SpotifyClone.albuns) AS 'albuns';
